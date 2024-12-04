52c08dc4af887e1bbc2f79830f7c86a737d6d77c
Kamila <ksalimzh@gmail.com>
2024-12-04 19:13:22 +0500

    Initial Commit

---
+++ vk_testApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk_testApp
+//
+//  Created by kamila on 04.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
